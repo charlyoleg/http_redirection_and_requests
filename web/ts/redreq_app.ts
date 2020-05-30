49fe0ef82663bb8e766dd6d885c660a0d85cce60
charli <charlyoleg@fabfolk.com>
2020-05-30 13:02:36 +0200

    button action

--- web/ts/redreq_app.ts
+++ web/ts/redreq_app.ts
@@ -3,3 +3,10 @@
 
 console.log("Hello from redreq_app.ts");
 
+function action_fetch () {
+  console.log("Hello from action_fetch()");
+  document.querySelector('#output_fetch').innerHTML = 'blabla';
+}
+
+document.querySelector('#btn_fetch').addEventListener('click', action_fetch);
+
