2b988dfaec31d92eebe4c36784a2fa5c5a9ba4c7
charli <charlyoleg@fabfolk.com>
2020-05-30 21:33:51 +0200

    using request

---
+++ nodejs_requests/request_requests.js
@@ -0,0 +1,16 @@
+// request_requests.js
+
+const request = require('request');
+
+// to avoid 'Error: self signed certificate'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+request.get('https://ZZZ.LocalHost:8443/aa', (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('payload: ' + body);
+
+});
+
--- scr/run_backend.sh
+++ scr/run_backend.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # run_backend.sh
 
+rm -fr backend
 git clone https://github.com/charlyoleg/host_registry.git backend/host_registry
 cd backend/host_registry
 npm i
